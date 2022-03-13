e71dbf2eb010e24218cdb8fc82fcbd1a04d88794
Hayden Frentzel <htfrentzel@gmail.com>
2022-03-12 21:01:18 -0600

    bashrc and tmux conf

--- setup.sh
+++ setup.sh
@@ -6,27 +6,22 @@ cd $BASEDIR
 # ln -s ${PWD}/vim/.vimrc ~/.vimrc
 # ln -s ${PWD}/vim/.vim ~/.vim
 
-vimrc=~/.vimrc
-if [ -e "$vimrc" ]; then
-    if [ -L "$vimrc" ]; then
-        echo "$vimrc already exists and is a link."
+add_symlink () {
+    src_file=$1
+    new_link=$2
+    if [ -e "$new_link" ]; then
+        if [ -L "$new_link" ]; then
+            echo "$new_link already exists and is a link."
+        else
+            echo "Error. $new_link already exists"
+                fi
     else
-        echo "Error. $vimrc already exists"
-    fi
-else
-    echo "$vimrc does not exist. Creating symlink..."
-    ln -s ${PWD}/vim/.vimrc ~/.vimrc
-fi
+        echo "$new_link does not exist. Creating symlink..."
+            ln -s $src_file $new_link
+            fi
+}
 
-
-vim=~/.vim
-if [ -e "$vim" ]; then
-    if [ -L "$vim" ]; then
-        echo "$vim already exists and is a link."
-    else
-        echo "Error. $vim already exists"
-    fi
-else
-    echo "$vim does not exist. Creating symlink..."
-    ln -s ${PWD}/vim/.vim ~/.vim
-fi
+add_symlink ${PWD}/vim/.vimrc ~/.vimrc
+add_symlink ${PWD}/vim/.vim ~/.vim
+add_symlink ${PWD}/tmux/.tmux.conf ~/.tmux.conf
+add_symlink ${PWD}/bash/.bashrc ~/.bashrc
