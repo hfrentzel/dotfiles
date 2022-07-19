93ad27fbeb29b64160191038f020d76e1d78bb9a
Hayden Frentzel <htfrentzel@gmail.com>
2022-07-18 20:55:03 -0500

    Setup workspaces and multiple language servers for python

--- vim/.vim/plugin/lsp.lua
+++ vim/.vim/plugin/lsp.lua
@@ -4,13 +4,35 @@ local on_attach = function ()
     vim.keymap.set('n', '<leader>3', "<cmd>lua vim.diagnostic.setloclist()<CR>", {buffer = true, silent = true})
 end
 
+local util = require('lspconfig/util')
+local path = util.path
+
+local py_before_init = function(params, config)
+    local match = vim.fn.glob(path.join(config.root_dir, '*', 'pyvenv.cfg'))
+    if match ~= '' then
+        config.settings.pylsp.plugins.jedi.environment = path.dirname(match)
+    end
+end
+
 require'lspconfig'.pylsp.setup{
+    name = 'pylsp',
     on_attach = on_attach,
+    before_init = py_before_init,
+    root_dir = function(fname)
+        if vim.g['workspaces'] then
+            for _, directory in pairs(vim.g['workspaces']) do
+                if vim.fn.stridx(fname, directory) > -1 then
+                    return directory
+                end
+            end
+        end
+    end,
     settings = {
         pylsp = {
             plugins = {
-                flake8 = {maxLineLength = 120}
+                flake8 = {maxLineLength = 120},
+                jedi = {environment = nil}
             }
         }
-    }
+    },
 }
--- vim/.vim/plugin/workspace_setup.vim
+++ vim/.vim/plugin/workspace_setup.vim
@@ -3,8 +3,13 @@ if !filereadable(s:filename)
     finish
 endif
 
+let g:workspaces = {}
 for line in readfile(s:filename)
     let s:array = split(line)
     if len(s:array) < 2 | continue | endif
-    execute "command Cd".s:array[0]." :cd ".s:array[1]
+    let g:workspaces[s:array[0]] = s:array[1]
+endfor
+
+for workspace in items(g:workspaces)
+    execute "command Cd".workspace[0]." :cd ".workspace[1]
 endfor
