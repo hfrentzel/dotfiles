97ffd5bd1992ea173b6c48425f714da777005340
Hayden Frentzel <htfrentzel@gmail.com>
2022-10-03 15:59:44 -0500

    fix conditions in git_stage.sh

--- git/git_stage.sh
+++ git/git_stage.sh
@@ -8,7 +8,7 @@ stageBranch() {
     currBranch=$(git branch --show-current)
 
     git pull origin $currBranch
-    if [[ $? -eq 0 ]]; then
+    if [[ $? -ne 0 ]]; then
         echo "Conflicts while pulling $currBranch. Resolve and press enter to continue "
         read NULL
         git commit -m "Merge origin"
@@ -20,7 +20,7 @@ stageBranch() {
     git pull
 
     git merge --no-ff $currBranch
-    if [[ $? -eq 0 ]]; then
+    if [[ $? -ne 0 ]]; then
         echo "Conflicts while merging $currBranch into $STAGE_BRANCH. Resolve and press enter to continue"
         read NULL
         git commit -m "Merge $currBranch into $STAGE_BRANCH"
