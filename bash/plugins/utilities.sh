7cbc46da63c3ac571512fb98641a4ac4ce8bd75c
Hayden Frentzel <htfrentzel@gmail.com>
2023-10-24 17:10:31 -0500

    Add conf function

--- bash/plugins/utilities.sh
+++ bash/plugins/utilities.sh
@@ -1,3 +1,27 @@
 rgl() {
     rg -p "$@" | less -RFXS
 }
+
+conf() {
+    TOOL=$1
+    DIR="$HOME/.config/$1"
+
+    if [[ ! -d "$DIR" ]]; then
+        echo "No config found for $1"
+        return
+    fi
+
+    FILES="$(find "$DIR" -mindepth 1)"
+    NUM_FILES=$(echo "$FILES" | wc -l)
+    if [[ $NUM_FILES -gt 1 ]]; then
+        # nvim $(echo "$FILES" | fzf)
+        echo "$FILES" | fzf --bind 'enter:become(nvim {}),ctrl-o:become(bat {})'
+    else
+        nvim $FILES
+    fi
+}
+
+_fzf_conf_completion() {
+    _fzf_complete -- "$@" < <(ls ~/.config/)
+}
+complete -F _fzf_conf_completion conf
