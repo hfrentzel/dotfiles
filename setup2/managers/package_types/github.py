aba1e894caaffcfbb251b06cee0f87dea5d7d7f2
Hayden Frentzel <htfrentzel@gmail.com>
2023-12-09 16:02:39 -0600

    Add specs for github and gitlab CLIs

--- setup2/managers/package_types/github.py
+++ setup2/managers/package_types/github.py
@@ -60,19 +60,24 @@ class Github():
             # TODO handle deb files when sudo permissions are available
             pass
 
+        asset_list = [a for a in asset_list if a.endswith('.zip') or '.tar.' in a]
+
         if any(system_os in a for a in asset_list):
             asset_list = [a for a in asset_list if system_os in a]
+        print(asset_list)
         if any(hardware in a for a in asset_list):
             asset_list = [a for a in asset_list if hardware in a]
+        print(asset_list)
         if any(hardware_alt in a for a in asset_list):
             asset_list = [a for a in asset_list if hardware_alt in a]
+        print(asset_list)
 
         if system_os == 'linux' and hardware == 'x86_64' and len(asset_list) == 2:
             asset_list = [a for a in asset_list if 'musl' in a] or asset_list
-            asset_list = [a for a in asset_list if '.tar.' in a]
 
         if len(asset_list) == 1:
             return asset_list[0]
+        print(asset_list)
         return None
 
     @classmethod
