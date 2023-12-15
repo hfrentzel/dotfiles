38cc048cb1254d5d269174ec3723a0f472cdc87e
Hayden Frentzel <htfrentzel@gmail.com>
2023-12-14 21:37:12 -0600

    Remove original setup_tools

--- languages/rust/__init__.py
+++
@@ -1,8 +0,0 @@
-
-def rust_editing():
-    # rustup install
-    # curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path
-
-    # rust-analyzer install
-    # https://rust-analyzer.github.io/manual.html#rust-analyzer-language-server-binary
-    pass
