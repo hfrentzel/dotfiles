901083b4575eafe42ba58774368d6adb5325c172
Hayden Frentzel <htfrentzel@gmail.com>
2024-03-04 20:37:14 -0600

    feat(env_setup) Support external specs
    Added the ability to load additional spec.jsons for files outside
    of the the dotfiles project. These specs are declared in USER_CONFIG
    in the 'external' map.

--- setup2/builder.py
+++ setup2/builder.py
@@ -1,6 +1,6 @@
 import json
 import os
-from typing import Dict, Any, Optional, Tuple, Type
+from typing import Dict, Any, Optional, Tuple, Type, List
 
 from setup2.managers import Exe, Sym, Command, Dir, Lib, Parser, Spec
 from setup2.conf import conf
@@ -18,32 +18,7 @@ TYPE_MAP: Dict[str, Type[Spec]] = {
 
 
 def build_resources(resource: Optional[str]) -> Optional[Tuple[Any, str]]:
-    with open(os.path.join(conf.dotfiles_home, 'main2.json'), encoding='utf-8') as f:
-        base_spec = json.loads(f.read())
-    addons = base_spec['addons']
-    specs = base_spec['resources']
-
-    choices = {}
-    if os.path.exists(USER_CONFIG):
-        with open(USER_CONFIG, encoding='utf-8') as f:
-            choices = json.loads(f.read())['addons']
-        if missing_addons := set(addons) - set(choices):
-            print('There are new addons that are not tracked on this machine')
-            for addon in missing_addons:
-                choices[addon] = input(f'Do you want to manage addon "{addon}"'
-                                       f' [y/n]?: ').lower().startswith('y')
-            write_config(choices)
-    else:
-        print('No env config file found. Creating one...')
-        choices = select_addons(addons)
-        os.makedirs(os.path.dirname(USER_CONFIG), exist_ok=True)
-        write_config(choices)
-
-    for name, file in addons.items():
-        if not choices[name]:
-            continue
-        with open(os.path.join(conf.dotfiles_home, file), encoding='utf-8') as f:
-            specs.update(json.loads(f.read()))
+    specs = collect_specs()
 
     single_spec = None
     for name, spec in specs.items():
@@ -63,9 +38,55 @@ def build_resources(resource: Optional[str]) -> Optional[Tuple[Any, str]]:
     return single_spec
 
 
-def write_config(choices: Dict[str, bool]) -> None:
+# TODO Add flag to optionally include all addons regardless of flags set in
+# USER_CONFIG
+def collect_specs() -> Dict[str, Dict[str, Any]]:
+    with open(os.path.join(conf.dotfiles_home, 'main2.json'), encoding='utf-8') as f:
+        base_spec = json.loads(f.read())
+    addons = base_spec['addons']
+    specs: Dict[str, Dict[str, Any]] = base_spec['resources']
+
+    choices, external_addons = load_config(addons)
+
+    for name, file in addons.items():
+        if not choices[name]:
+            continue
+        with open(os.path.join(conf.dotfiles_home, file), encoding='utf-8') as f:
+            specs.update(json.loads(f.read()))
+
+    for e_addon in external_addons or []:
+        with open(os.path.expanduser(e_addon), encoding='utf-8') as f:
+            specs.update(json.loads(f.read()))
+
+    return specs
+
+
+def load_config(addons: Dict[str, str]) -> Tuple[Dict[str, bool], Optional[List[str]]]:
+    if os.path.exists(USER_CONFIG):
+        with open(USER_CONFIG, encoding='utf-8') as f:
+            config_options = json.loads(f.read())
+            choices = config_options['addons']
+            external_addons = config_options.get('external')
+        if missing_addons := set(addons) - set(choices):
+            print('There are new addons that are not tracked on this machine')
+            for addon in missing_addons:
+                choices[addon] = input(f'Do you want to manage addon "{addon}"'
+                                       f' [y/n]?: ').lower().startswith('y')
+            write_config(choices, external_addons)
+        return choices, external_addons
+
+    print('No env config file found. Creating one...')
+    choices = select_addons(addons)
+    os.makedirs(os.path.dirname(USER_CONFIG), exist_ok=True)
+    write_config(choices)
+
+    return choices, None
+
+
+def write_config(choices: Dict[str, bool],
+                 external_addons: Optional[List[str]] = None) -> None:
     with open(USER_CONFIG, 'w', encoding='utf-8') as f:
-        f.write(json.dumps({'addons': choices}, indent=4))
+        f.write(json.dumps({'addons': choices, 'external': external_addons}, indent=4))
 
 
 def select_addons(addons: Dict[str, str]) -> Dict[str, bool]:
