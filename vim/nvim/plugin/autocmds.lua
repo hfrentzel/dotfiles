784dd1869b96996202e7e24f0fea4193e862feee
Hayden Frentzel <htfrentzel@gmail.com>
2024-03-13 21:48:05 -0500

    fix(vim) Stop intro from flickering on startup
    Fixes a long-standing issue where the vim intro screen would
    immediately disappear after startup. This was caused by the FocusGained
    autocommand running, triggering colorscheme and winhighlight updates
    that redraw the screen. Interestingly the issue doesn't happen outside
    tmux because the FocusGained event doesn't occur on startup then.
    
    To fix, FocusGained is `eventignore`d on startup, and then reenable on
    the first FocusLost.

--- vim/nvim/plugin/autocmds.lua
+++ vim/nvim/plugin/autocmds.lua
@@ -1,5 +1,16 @@
 local focus = require('my_lua.focus')
 
+vim.o.eventignore='FocusGained'
+vim.api.nvim_create_augroup('firstunfocus', {clear = true})
+vim.api.nvim_create_autocmd('FocusLost', {
+    group = 'firstunfocus',
+    pattern = '*',
+    callback = function()
+        vim.o.eventignore=''
+        vim.api.nvim_del_augroup_by_name('firstunfocus')
+    end
+})
+
 vim.api.nvim_create_augroup('focus', {clear = true})
 vim.api.nvim_create_autocmd('FocusGained', {
     group = 'focus',
@@ -21,3 +32,5 @@ vim.api.nvim_create_autocmd('WinLeave', {
     pattern = '*',
     callback = focus.blur_window
 })
+
+focus.focus_window()
