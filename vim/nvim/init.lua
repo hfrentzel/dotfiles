9e96486b03a1826d38f2fd10bfac86405fa3489b
Hayden Frentzel <htfrentzel@gmail.com>
2024-03-13 21:57:07 -0500

    feat(vim) disable ruby, node, and perl providers

--- vim/nvim/init.lua
+++ vim/nvim/init.lua
@@ -1,5 +1,8 @@
 vim.o.packpath = vim.o.runtimepath
 vim.g.python3_host_prog = vim.fn.trim(vim.fn.system('which python'))
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
 vim.cmd.source('~/.config/vim/vimrc')
 
 vim.o.guicursor='n-v-c-sm:block-blinkon500-blinkwait200,i-ci-ve:ver25-blinkon500,r-cr-o:hor20-blinkon500'
