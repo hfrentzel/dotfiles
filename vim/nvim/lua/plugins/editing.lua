ce232bbe3aaae6feefe6b608024836508121ed23
Hayden Frentzel <htfrentzel@gmail.com>
2024-03-20 20:26:55 -0500

    fix(vim) Add indent settings back as defaults

--- vim/nvim/lua/plugins/editing.lua
+++ vim/nvim/lua/plugins/editing.lua
@@ -3,7 +3,13 @@
 -- Currently includes vim-surround and CamelCaseMotion
 
 return {
-    {'vim-sleuth', dir='~/.config/nvim/pack/vendor/opt/vim-sleuth'},
+    {'vim-sleuth', dir='~/.config/nvim/pack/vendor/opt/vim-sleuth',
+        init = function()
+            vim.o.expandtab = true
+            vim.o.shiftwidth = 4
+            vim.o.tabstop = 4
+        end
+    },
     {'vim-surround', dir='~/.config/nvim/pack/vendor/opt/vim-surround',
         event='VeryLazy'},
     {'CamelCaseMotion', dir='~/.config/nvim/pack/vendor/opt/CamelCaseMotion',
