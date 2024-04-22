9fec56ba0a47c71f6f9f48441f90c260188e3421
Hayden Frentzel <htfrentzel@gmail.com>
2024-04-21 21:07:03 -0500

    fix(vim) Add fold_size to foldtext
    Ports foldtext to lua and makes it more robust, including the
    number of lines in the fold to the foldtext.
    
    In order to properly right-align the fold size I needed to know the
    width of the gutter, found a key snippet for this here:
    https://github.com/neovim/neovim/issues/17229#issuecomment-1024847971

--- vim/nvim/after/plugin/folding.vim
+++
@@ -1,6 +0,0 @@
-hi Folded  term=bold ctermfg=1 ctermbg=242
-
-function! MyFoldText() abort
-    return getline(v:foldstart)
-endfunction
-set foldtext=MyFoldText()
---
+++ vim/nvim/after/plugin/folds.lua
@@ -0,0 +1,19 @@
+-- folds.lua
+-- A custom foldtext function that shows the first line in the fold and the
+-- number of lines inside
+
+_G.foldtext = function()
+    local line = vim.fn.getline(vim.v.foldstart)
+    local fold_size = (vim.v.foldend - vim.v.foldstart + 1) .. ' lines'
+    local width = vim.fn.winwidth(0) - vim.fn.getwininfo(vim.fn.win_getid())[1].textoff - string.len(fold_size)
+
+    if string.len(line) > width then
+        line = string.sub(line, 1, width)
+    elseif string.len(line) < width then
+        line = line .. string.rep("·", width - string.len(line))
+    end
+
+    return line .. fold_size
+end
+
+vim.opt.foldtext = 'v:lua.foldtext()'
