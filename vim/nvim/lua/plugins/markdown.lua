9ce70354e3498be26727b62ec332fa553001fdaa
Hayden Frentzel <htfrentzel@gmail.com>
2024-06-25 20:06:30 -0500

    fix(vim) Launch markdown-preview.nvim on filetype
    The plugin doesn't seem to work when used with command-based lazy
    loading, so I'm switching to filetype-based loading.

--- vim/nvim/lua/plugins/markdown.lua
+++ vim/nvim/lua/plugins/markdown.lua
@@ -2,6 +2,6 @@ return {
     {
         'markdown-preview.nvim',
         dir = '~/.config/nvim/pack/vendor/opt/markdown-preview.nvim/',
-        cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+        filetype = { 'markdown' },
     },
 }
