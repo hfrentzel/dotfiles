15cf30abca07858fa01af1a1859af70aea55ec53
Hayden Frentzel <htfrentzel@gmail.com>
2024-10-11 10:56:03 -0500

    chore(submodules) Update fugitive, gitsigns.nvim

--- vim/nvim/lua/plugins/git.lua
+++ vim/nvim/lua/plugins/git.lua
@@ -38,22 +38,13 @@ return {
         opts = {
             signs = {
                 add = {
-                    hl = 'GitSignsAdd',
                     text = '+',
-                    numhl = 'GitSignsAddNr',
-                    linehl = 'GitSignsAddLn',
                 },
                 change = {
-                    hl = 'GitSignsChange',
                     text = '~',
-                    numhl = 'GitSignsChangeNr',
-                    linehl = 'GitSignsChangeLn',
                 },
                 delete = {
-                    hl = 'GitSignsDelete',
                     text = '-',
-                    numhl = 'GitSignsDeleteNr',
-                    linehl = 'GitSignsDeleteLn',
                 },
             },
             on_attach = function(bufnr)
