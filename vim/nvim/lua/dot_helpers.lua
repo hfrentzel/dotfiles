ce8321b0839925c21b2cc60c5ca64d903a0e65c9
Hayden Frentzel <htfrentzel@gmail.com>
2024-12-30 16:56:44 -0600

    fix(vim) Fix root_dir resolution for pylsp

--- vim/nvim/lua/dot_helpers.lua
+++ vim/nvim/lua/dot_helpers.lua
@@ -12,7 +12,7 @@ end
 
 functions.get_git_root = function()
     local git_prefix = 'git -C ' .. vim.fn.expand('%:p:h')
-    if vim.fn.system(git_prefix .. ' rev-parse  --is-inside-work-tree') == 'true' then
+    if vim.fn.system(git_prefix .. ' rev-parse  --is-inside-work-tree') == 'true\n' then
         return vim.fn.substitute(
             vim.fn.system(git_prefix .. ' rev-parse --show-toplevel'),
             '\n',
