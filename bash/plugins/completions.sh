b29f913aacbbc507ab58e4aeebe07262f212dd29
Hayden Frentzel <htfrentzel@gmail.com>
2025-04-24 13:36:09 -0500

    perf(bash) Remove completion inits from bash startup
    The `which` calls in these completion setups were costing a couple
    hundred milliseconds during bash startup. They can be removed without
    issue as I am not using any of these commands currently, plus:
    * when I starting using `jira` again, it will be with build-hole which
    will include the completion file
    * I don't know that I'll ever use `aws` again, if I do I can find a
      better solution
    * same with `gh` maybe create a completion resource manager to populate
      a completion file.

--- bash/plugins/completions.sh
+++ bash/plugins/completions.sh
@@ -1,14 +1,2 @@
 
 test -f ~/.test_configs && complete -W "$(jq -r 'keys[]' ~/.test_configs)" tests
-
-if [[ -x $(which aws) && -x $(which aws_completer) ]]; then
-    complete -C "aws_completer" aws
-fi
-
-if [[ -x $(which jira) ]]; then
-    source <(jira completion bash)
-fi
-
-if [[ -x $(which gh) ]]; then
-    eval "$(gh completion -s bash)"
-fi
