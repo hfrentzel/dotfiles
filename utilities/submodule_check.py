d53ec58cfdc8fc843d8ae9b711728652fddb7699
Hayden Frentzel <htfrentzel@gmail.com>
2025-05-21 19:55:00 -0500

    feat(setup) Create submodule check script
    grep is not installed on Windows by default, so the old submodules
    resource check would never succeed. I've instead written a python script
    to check the output, so this will be more cross-platform friendly.

---
+++ utilities/submodule_check.py
@@ -0,0 +1,15 @@
+# This is a utility script meant to be used by the setup submodules resource
+# to check whether any git submodules are out of date. It does this by running
+# `git submodule status` and then looking for any `+` or `~` markers on the
+# output.
+import subprocess
+import sys
+
+result = subprocess.run(["git", "submodule", "status"], capture_output=True)
+
+lines = result.stdout.decode().split("\n")
+
+if any(not line.startswith(" ") for line in lines):
+    sys.exit(1)
+else:
+    sys.exit(0)
