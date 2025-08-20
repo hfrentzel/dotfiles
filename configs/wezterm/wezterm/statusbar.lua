3e94fb79725db138137bc7aa9c5c9265d50b090b
Hayden Frentzel <htfrentzel@gmail.com>
2025-08-19 20:34:06 -0500

    feat(wezterm) Include domain in tab title
    Domain is only visible if more than one domain is active in the
    workspace.

--- configs/wezterm/wezterm/statusbar.lua
+++ configs/wezterm/wezterm/statusbar.lua
@@ -2,6 +2,7 @@ local wezterm = require('wezterm')
 local config = {}
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
+config.tab_max_width = 50
 
 local colon = ':'
 wezterm.on('update-right-status', function(win, _)
@@ -15,6 +16,26 @@ wezterm.on('update-right-status', function(win, _)
     local workspace = '(' .. win:active_workspace() .. ')'
 
     win:set_right_status(workspace .. ' ' .. name .. ' ' .. date)
+end)
+
+wezterm.on('format-tab-title', function(tab, tabs, _, _, _, _)
+    local index = tab.tab_index + 1
+    local title = tab.active_pane.title
+
+    local domain = tab.active_pane.domain_name
+    local show_domain = false
+    for _, tabi in ipairs(tabs) do
+        if tabi.active_pane.domain_name ~= domain then
+            show_domain = true
+            break
+        end
+    end
+
+    if show_domain then
+        return ' '..index..': ('..domain..') '..title..' '
+    else
+        return ' '..index..': '..title..' '
+    end
 end)
 
 return config
