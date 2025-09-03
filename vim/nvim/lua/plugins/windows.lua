00520b0b746842e47121fa1474dbae4308b37ac9
Hayden Frentzel <htfrentzel@gmail.com>
2025-09-02 21:41:15 -0500

    perf(vim) Improve pane navigation in wezterm
    The smart-splits.nvim integration for wezterm uses 6 wezterm cli calls
    everytime a `move` command is called. For whatever reason (I suspect
    antivirus effects), these clis calls have a non-negligible delay on some
    Window machines, resulting in navigation from a vim pane into another
    pane incredibly slow. After looking at smart-splits' code, I figured I
    could vastly simplify the move commands for my use case, speeding them
    up greatly.
    
    I may need to do this for the resize commands too, but as of yet have
    not used those enough that it feels needed.

--- vim/nvim/lua/plugins/windows.lua
+++ vim/nvim/lua/plugins/windows.lua
@@ -2,6 +2,23 @@
 -- Keymaps for navigating and managing windows and panes, interoperate with
 -- wezterm and tmux using smart-splits.nvim
 
+local directions = {
+    left = "h",
+    down = "j",
+    up = "k",
+    right = "l"
+}
+
+local function move_pane(dir)
+    return function()
+        local start_win = vim.fn.winnr()
+        vim.cmd.wincmd(directions[dir])
+        if vim.fn.winnr() == start_win then
+            vim.fn.system({'wezterm', 'cli', 'activate-pane-direction', dir})
+        end
+    end
+end
+
 return {
     {
         'smart-splits.nvim',
@@ -11,10 +28,18 @@ return {
             splits.setup({
                 at_edge = 'stop',
             })
-            vim.keymap.set('n', '<C-h>', splits.move_cursor_left)
-            vim.keymap.set('n', '<C-j>', splits.move_cursor_down)
-            vim.keymap.set('n', '<C-k>', splits.move_cursor_up)
-            vim.keymap.set('n', '<C-l>', splits.move_cursor_right)
+
+            if vim.trim((vim.env.TERM_PROGRAM or ''):lower()) == 'wezterm' then
+                vim.keymap.set('n', '<C-h>', move_pane("left"))
+                vim.keymap.set('n', '<C-j>', move_pane("down"))
+                vim.keymap.set('n', '<C-k>', move_pane("up"))
+                vim.keymap.set('n', '<C-l>', move_pane("right"))
+            else
+                vim.keymap.set('n', '<C-h>', splits.move_cursor_left)
+                vim.keymap.set('n', '<C-j>', splits.move_cursor_down)
+                vim.keymap.set('n', '<C-k>', splits.move_cursor_up)
+                vim.keymap.set('n', '<C-l>', splits.move_cursor_right)
+            end
 
             vim.keymap.set('n', '<M-h>', splits.resize_left)
             vim.keymap.set('n', '<M-j>', splits.resize_down)
