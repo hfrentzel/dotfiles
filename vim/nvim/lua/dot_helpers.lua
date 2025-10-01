5a0a6a94f5049061a42af80c5de4de8ac229d38c
Hayden Frentzel <htfrentzel@gmail.com>
2025-10-01 18:44:10 -0500

    fix(vim) Detect python version greater than 3.9

--- vim/nvim/lua/dot_helpers.lua
+++ vim/nvim/lua/dot_helpers.lua
@@ -2,7 +2,7 @@ local functions = {}
 
 functions.get_python_major_version = function(pyvenv_file)
     for line in io.lines(pyvenv_file) do
-        local version = vim.fn.matchstr(line, 'version = \\zs\\d.\\d')
+        local version = vim.fn.matchstr(line, 'version = \\zs\\d.\\d\\+')
         if version ~= '' then
             return version
         end
