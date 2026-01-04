4eeb6e41e05f04414a7f3876614b8f4adca93ae1
Hayden Frentzel <htfrentzel@gmail.com>
2026-01-04 17:38:35 -0600

    feat(vim) Add command to switch between source and header files

--- vim/nvim/lsp/clangd.lua
+++ vim/nvim/lsp/clangd.lua
@@ -1,3 +1,19 @@
+local function switch_source_header(bufnr, client)
+    local method_name = 'textDocument/switchSourceHeader'
+    local params = vim.lsp.util.make_text_document_params(bufnr)
+
+    client:request(method_name, params, function(err, result)
+        if err then
+            error(tostring(err))
+        end
+        if not result then
+            vim.notify('corresponding file cannot be determined')
+            return
+        end
+        vim.cmd.edit(vim.uri_to_fname(result))
+    end, bufnr)
+end
+
 return {
     cmd = {
         'clangd',
@@ -19,4 +35,9 @@ return {
         completeUnimported = true,
         clangdFileStatus = true,
     },
+    on_attach = function(client, bufnr)
+        vim.api.nvim_buf_create_user_command(bufnr, 'Switch', function()
+            switch_source_header(bufnr, client)
+        end, {})
+    end,
 }
