ca1e76620661d11175a9da19e0fcd80ecc186d94
Hayden Frentzel <htfrentzel@gmail.com>
2026-06-17 20:25:51 -0500

    vim: Improve Hex2Dec search highlighting
    * Save and restore any existing search highlight when running Hex2Dec
    * Don't throw an error if no valid hex values are in range

--- vim/nvim/plugin/hex.lua
+++ vim/nvim/plugin/hex.lua
@@ -1,13 +1,21 @@
 -- https://vim.fandom.com/wiki/Convert_between_hex_and_decimal
 vim.api.nvim_create_user_command('Hex2dec', function(args)
     if args.args == "" then
+        local save_search = vim.fn.getreg('/')
+        local search_active = vim.v.hlsearch
+
         local cmd
         if vim.startswith(vim.fn.histget(':', -1), "^'<,'>") and vim.fn.visualmode() == 'V' then
-            cmd = 's/%V0x\\x+/=submatch(0)+0/g'
+            cmd = 's/%V0x\\x+/=submatch(0)+0/ge'
         else
-            cmd = 's/0x\\x\\+/\\=submatch(0)+0/g'
+            cmd = 's/0x\\x\\+/\\=submatch(0)+0/ge'
         end
         vim.fn.execute(args.line1..','..args.line2..cmd)
+
+        vim.fn.setreg('/', save_search)
+        if search_active == 0 then
+            vim.cmd.nohlsearch()
+        end
     else
         if vim.startswith(args.args, '0x') then
             print(args.args + 0)
