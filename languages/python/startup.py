af0eee3cb0a857e2d4a8c2febf283b2cacc8e626
Hayden Frentzel <htfrentzel@gmail.com>
2026-06-18 17:43:57 -0500

    python: Fix deprecation error in startup.py
    Use `packaging` instead of `pkg_resources`

--- languages/python/startup.py
+++ languages/python/startup.py
@@ -1,17 +1,19 @@
 import atexit
 import os
 import sys
+
+from packaging.version import parse
 from pip._internal.operations.freeze import freeze
-from pkg_resources import parse_version as parse
 
 try:
     import readline
+
     try:
         readline.parse_and_bind("tab: complete")
     except ImportError:
         pass
 
-    if hasattr(sys, '__interactivehook__'):
+    if hasattr(sys, "__interactivehook__"):
         del sys.__interactivehook__
 
     histfile = os.path.expanduser("~/.local/share/python/history")
@@ -27,11 +29,11 @@ try:
 except ImportError:
     pass
 
-if prompt := os.getenv('PYPROMPT', None):
-    sys.ps1 = prompt + '> '
+if prompt := os.getenv("PYPROMPT", None):
+    sys.ps1 = prompt + "> "
 else:
-    sys.ps1 = f'py{sys.version_info[0]}.{sys.version_info[1]}> '
-sys.ps2 = '.' * (len(sys.ps1) - 1) + ' '
+    sys.ps1 = f"py{sys.version_info[0]}.{sys.version_info[1]}> "
+sys.ps2 = "." * (len(sys.ps1) - 1) + " "
 
 
 class Quitter:
@@ -41,6 +43,9 @@ class Quitter:
     def __repr__(self):
         sys.exit(self.num)
 
+    def __call__(self):
+        sys.exit(self.num)
+
 
 ch = Quitter(5)
 q = Quitter()
@@ -48,7 +53,6 @@ exit = q
 
 
 class Pip:
-
     @staticmethod
     def freeze():
         print(Pip.get_freeze())
@@ -58,34 +62,34 @@ class Pip:
         # installed_packages = {(p := pv.split('=='))[0].lower(): p[1] for pv in freeze() if '==' in pv}
         installed_packages = {}
         for pv in freeze():
-            if '==' in pv:
-                p = pv.split('==')
+            if "==" in pv:
+                p = pv.split("==")
                 installed_packages[p[0].lower()] = p[1]
 
         return installed_packages
 
 
 def get_requirements(filename, desired):
-    with open(filename, encoding='utf-8') as f:
+    with open(filename, encoding="utf-8") as f:
         for line in f.readlines():
-            if line.startswith('-r'):
-                get_requirements(line[:-1].split(' ')[1], desired)
-            elif line.startswith('git+'):
+            if line.startswith("-r"):
+                get_requirements(line[:-1].split(" ")[1], desired)
+            elif line.startswith("git+"):
                 continue
             else:
-                parsed = line[:-1].split('==')
+                parsed = line[:-1].split("==")
                 if len(parsed) == 2:
                     desired[parsed[0]] = parsed[1]
                 else:
                     desired[parsed[0]] = None
 
 
-if 'venv' in sys.executable:
-    os.chdir(os.path.join(os.path.dirname(sys.executable), '..', '..'))
+if "venv" in sys.executable:
+    os.chdir(os.path.join(os.path.dirname(sys.executable), "..", ".."))
 
     reqs: dict = {}
-    if os.path.exists('requirements.txt'):
-        get_requirements('requirements.txt', reqs)
+    if os.path.exists("requirements.txt"):
+        get_requirements("requirements.txt", reqs)
         curr_installed = Pip.get_freeze()
         not_installed = []
         out_of_date = []
@@ -96,10 +100,10 @@ if 'venv' in sys.executable:
                 out_of_date.append(package)
 
         if out_of_date:
-            print('The following modules packages are out of date:')
-            print(', '.join(out_of_date))
+            print("The following modules packages are out of date:")
+            print(", ".join(out_of_date))
         if not_installed:
-            print('The following modules are not installed')
-            print(', '.join(not_installed))
+            print("The following modules are not installed")
+            print(", ".join(not_installed))
         if not (out_of_date or not_installed):
-            print('All packages are up to date.')
+            print("All packages are up to date.")
